2340f71f9b13552d6d90b68125edd21d884211e6
Jacob Lewis <jacob@jaso5.com>
2025-05-19 02:55:01 +0100

    Fix MQTT

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ import logging
 
 from printers.printer import Printer
 
-mqtt = mqtt.Client()
+mqtt = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
 mqtt.connect("mqtt.hacklab")
 
 logger = logging.getLogger()
@@ -32,15 +32,22 @@ def main():
 
     config_path: str = parse_args()
 
+    logger.info("Loading config")
+
     try:
         config = json.load(open(config_path))
     except Exception as err:
         logger.error(f"Could not load config: {err}")
         exit(-1)
 
+    logger.info("Printer init")
+
     for printer_config in config["printers"]:
         printer = Printer(printer_config)
+        printer.connect()
         printers.append(printer)
+
+    logger.info("Running")
 
     while True:
         time.sleep(10)
